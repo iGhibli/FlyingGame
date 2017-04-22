319185fb51ab7cd1000213eea8c6012cc64b4d51
iGhibli <iGhibli@163.com>
2017-04-22 18:16:32 +0800

    添加title和start按钮

--- FlyingGame/src/app/scenes/MainScene.lua
+++ FlyingGame/src/app/scenes/MainScene.lua
@@ -7,6 +7,22 @@ function MainScene:ctor()
     display.newSprite("image/main.jpg")
 		:pos(display.cx, display.cy)
 	    :addTo(self)
+	local title = display.newSprite("image/title.png")
+		:pos(display.cx / 2 * 3, display.cy)
+    	:addTo(self)
+    local move1 = cc.MoveBy:create(0.5,cc.p(0,10))
+    local move2 = cc.MoveBy:create(0.5,cc.p(0,-10))
+    local SequenceAction = cc.Sequence:create(move1,move2)
+    transition.execute(title, cc.RepeatForever:create(SequenceAction))
+
+    cc.ui.UIPushButton.new({ normal = "image/start1.png", pressed = "image/start2.png"})
+    	:onButtonClicked(function ()
+    		-- body
+    		printf("start")
+
+    	end)
+    	:pos(display.cx/2, display.cy)
+    	:addTo(self)
 end
 
 function MainScene:onEnter()
