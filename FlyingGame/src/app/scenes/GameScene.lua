f5a50599e38d854353c6cd71d7199d2e12bb405a
iGhibli <iGhibli@163.com>
2017-04-23 19:00:01 +0800

    引入物理引擎

--- FlyingGame/src/app/scenes/GameScene.lua
+++ FlyingGame/src/app/scenes/GameScene.lua
@@ -1,9 +1,13 @@
 local GameScene = class("GameScene", function (  )
 	-- body
-	return display.newScene("GameScene")
+	return display.newPhysicsScene("GameScene")
 end)
 
 function GameScene:ctor()
+	self.world = self:getPhysicsWorld()
+	self.world:setGravity(cc.p(0, -98.0))
+	self.world:setDebugDrawMask(cc.PhysicsWorld.DEBUGDESW_ALL)
+
 	local backgroundLayer = BackgroundLayer.new()
         :addTo(self)
 end
