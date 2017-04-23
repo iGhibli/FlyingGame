6cfcb61608b1bc5d487cc62e64cdfd92fcf99ad7
iGhibli <iGhibli@163.com>
2017-04-23 19:25:54 +0800

    解决因资源文件缺少导致无法正常运行的问题

--- FlyingGame/src/app/scenes/GameScene.lua
+++ FlyingGame/src/app/scenes/GameScene.lua
@@ -4,12 +4,15 @@ local GameScene = class("GameScene", function (  )
 end)
 
 function GameScene:ctor()
+	-- 获取场景绑定的物理世界对象, 获取的 self.world 默认是有带重力的, 大小为(0.0f, -98.0f)
 	self.world = self:getPhysicsWorld()
+	-- 通过的setGravity()方法来改变重力大小
 	self.world:setGravity(cc.p(0, -98.0))
-	self.world:setDebugDrawMask(cc.PhysicsWorld.DEBUGDESW_ALL)
-
+	-- setDebugDrawMask 方法是在调试物理世界时使用的，该方法可以开启调试模
+	-- 能把物理世界中不可见的body，shape，joint等元素可视化。当调试结束时，需要把该功能关闭。
+	self.world:setDebugDrawMask(cc.PhysicsWorld.DEBUGDRAW_ALL)
 	local backgroundLayer = BackgroundLayer.new()
-        :addTo(self)
+		:addTo(self)
 end
 
 function GameScene:onEnter()
